234a59c50fee8da9cdb3eb5d435325b3838d7f8f
David Stanek <dstanek@dstanek.com>
2009-11-07 00:03:09 -0500

    removed the last reference to peak since we are trying to limit dependencies

--- snakeguice/interceptors.py
+++ snakeguice/interceptors.py
@@ -1,8 +1,3 @@
-#!python
-
-
-from peak.util.decorators import decorate_assignment
-
 
 class ParameterInterceptor(object):
 
@@ -13,11 +8,11 @@ class ParameterInterceptor(object):
         #TODO: add a test for more than one kwargs
         (param_name, param_type) = kwargs.items()[0]
 
-        def callback(frame, name, func, old_locals):
+        def callback(method):
             def _f(*args, **kwargs): # it is OK to override the outer function
                 kwargs[param_name] = self._injector.get_instance(
                         param_type, annotation)
-                return func(*args, **kwargs)
+                return method(*args, **kwargs)
             return _f
 
-        return decorate_assignment(callback)
+        return callback
