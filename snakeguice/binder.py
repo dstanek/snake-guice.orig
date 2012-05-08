351a1007442736e6adb273791f4a2969526b7730
David Stanek <dstanek@dstanek.com>
2012-05-07 21:12:42 -0400

    Adds a lazy binder for offline validation
    This new lazy binding can be used in tools to detect if bindings
    are correct. An official snake-guice tool is coming soon!

--- snakeguice/binder.py
+++ snakeguice/binder.py
@@ -28,12 +28,7 @@ class Binder(object):
 
     def __init__(self, parent=None):
         self._parent = parent or _EmptyBinder()
-        self._errors = []
         self._binding_map = {}
-
-    def add_error(self, exc, msg):
-        self._errors.append(exc(msg))
-        #TODO: do something with this
 
     def bind(self, _class, **kwargs):
         key = Key(interface=_class, annotation=kwargs.get('annotated_with'))
@@ -73,9 +68,77 @@ class Binder(object):
         return Binder(self)
 
 
+class LazyBinder(object):
+
+    def __init__(self, parent=None):
+        self._parent = parent or _EmptyBinder()
+        self._binding_map = {}
+        self._errors = []
+
+    def add_error(self, msg):
+        import inspect
+        frame_rec = inspect.stack()[2]
+
+        location = 'File {0}, line {1}, in {2}'.format(
+                frame_rec[1], frame_rec[2], frame_rec[3])
+        source = frame_rec[4][frame_rec[5]].strip()
+        self._errors.append(BinderErrorRecord(message=msg,
+                                              location=location,
+                                              source=source))
+
+    @property
+    def errors(self):
+        return self._errors
+
+    def bind(self, _class, **kwargs):
+        key = Key(interface=_class, annotation=kwargs.get('annotated_with'))
+
+        binding = Binding()
+        binding.key = key
+        binding.scope = kwargs.get('in_scope', scopes.NO_SCOPE)
+
+        if key in self._binding_map:
+            self.add_error('baseclass %r already bound' % _class)
+
+        if 'to' in kwargs:
+            if not isinstance(kwargs['to'], type):
+                self.add_error('to requires a new-style class')
+
+            binding.provider = providers.create_simple_provider(kwargs['to'])
+
+        elif 'to_provider' in kwargs:
+            #TODO: add some validation
+            provider = kwargs['to_provider']
+            binding.provider = provider
+
+        elif 'to_instance' in kwargs:
+            if not isinstance(kwargs['to_instance'], object):
+                self.add_error('to_instance requires an instance of a '
+                               'new-style class')
+
+            provider = kwargs['to_instance']
+            binding.provider = providers.create_instance_provider(provider)
+
+        self._binding_map[key] = binding
+
+    def get_binding(self, key):
+        return self._binding_map.get(key) or self._parent.get_binding(key)
+
+    def create_child(self):
+        return Binder(self)
+
+
 class Binding(object):
 
-    def __init__(self):
-        self.key = None
-        self.provider = None
-        self.scope = None
+    def __init__(self, key=None, provider=None, scope=None):
+        self.key = key
+        self.provider = provider
+        self.scope = scope
+
+
+class BinderErrorRecord(object):
+
+    def __init__(self, message, location, source):
+        self.message = message
+        self.location = location
+        self.source = source
